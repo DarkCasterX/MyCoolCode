9a90afe3164e7c865b67149caf01dc1aabe7170e
DarkCasterX <53799437+DarkCasterX@users.noreply.github.com>
2021-01-03 15:56:21 -0500

    Add files via upload

---
+++ LinkedList.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct List
+{
+    int data;
+    struct List* rest;
+}List;
+
+List* CreateList(int val, List* list);
+
+List* EmptyList();
+
+int isEmpty(List* empty);
+
+int length(List* list, int startPos);
+
+List* searchPos(List* list, int start, int pos);
+
+List* last(List* list);
+
+void insert(List* list, int val, int pos);
+
+List* join_head(List* list, int val);
+
+void join_list(List* first, List* second);
+
+int DisplayList(List* display);
