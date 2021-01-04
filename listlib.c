e78883541721f0c414c3fa89dd9cd62ec2f84241
DarkCasterX <53799437+DarkCasterX@users.noreply.github.com>
2021-01-04 11:27:13 -0500

    Update listlib.c

--- listlib.c
+++ listlib.c
@@ -9,17 +9,24 @@ List* CreateList(int val, List* list)
     return newlist;
 }
 
-//Creates a list of a specified size filled with the 'val' argument
-List* ListConstruct(int val, int start, int size)
+//Recursive function used to implement ListConstruct
+List* ListBuilder(int val, int start, int size)
 {
     if(start < size - 1)
     {
-        return CreateList(val, ListConstruct(val, start + 1, size));
+        return CreateList(val, ListBuilder(val, start + 1, size));
     }
     else if( start >= size - 1)
     {
         return CreateList(val, EmptyList());
     }
+}
+
+
+//Creates a list of a specified size filled with the 'val' argument
+List* ListConstruct(int val, int size)
+{
+    ListBuilder(val, 0, size);
 }
 
 //Useful function to create an empty list
@@ -38,6 +45,7 @@ int isEmpty(List* empty)
     return 0;
 }
 
+//Recursive function used to implement list_length
 int length(List* list, int startPos)
 {
     if(isEmpty(list))
@@ -53,8 +61,7 @@ int list_length(List* list)
     length(list, 0);
 }
 
-//Searches for the element in the specified position, set start to zero unless
-//You otherwise need to
+//Recursive Function used to implement searchPos
 List* getPosition(List* list, int start, int pos)
 {
     if(start == pos)
@@ -64,6 +71,8 @@ List* getPosition(List* list, int start, int pos)
     getPosition(list->rest, start + 1, pos);
 }
 
+//Searches for the element in the specified position, set start to zero unless
+//You otherwise need to
 List* searchPos(List* list, int pos)
 {
     return getPosition(list, 0, pos);
@@ -90,24 +99,29 @@ void insert(List* list, int val, int ind)
     List* prev = searchPos(list, ind - 1);
     List* next = prev->rest;
     prev->rest = CreateList(val, next);
-    printf("Inserted element into list.\n");
+    printf("Inserted element into list at position %d\n", ind);
 
     //Set pointers to null before returning
 }
 
 //Returns a new list with the sspecified value at the start
-List* join_head(List* list, int val)
+void join_head(List* list, int val)
 {
-    return CreateList(val, list);
+    join_list(CreateList(val, EmptyList()), list);
 }
 
 //Add the elements of the second list to the first
 void join_list(List* first, List* second)
 {
-    List* connect = last(first);
-    connect->rest = second;
-
-    //Set pointers to null before returning
+    if(!isEmpty(first->rest))
+    {
+        List* connect = last(first);
+        connect->rest = second;
+    }
+    else if(isEmpty(first->rest))
+    {
+        first->rest = second;
+    }
 }
 
 //Adds an element to the end of the list
