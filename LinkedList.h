fd7a25e1dff667e08aa2c8eb580d0ae411bee091
DarkCasterX <53799437+DarkCasterX@users.noreply.github.com>
2021-01-04 21:46:14 -0500

    Updated ListDestory() function
    Changed it from a while loop to be recursive, because that's just my style. Thankfully, I don't have to update the header file as well this time.

--- LinkedList.h
+++ LinkedList.h
@@ -1,43 +1,173 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define nullptr 0
+#include "LinkedList.h"
 
-typedef struct List
+//Constructor to create a list
+List* CreateList(int val, List* list)
 {
-    int data;
-    struct List* rest;
-}List;
+    List* newlist = malloc(sizeof(List));
+    newlist->data = val;
+    newlist->rest = list;
+    return newlist;
+}
 
-List* CreateList(int val, List* list);
+//Recursive function used to implement ListConstruct
+List* ListBuilder(int val, int start, int size)
+{
+    if(start < size - 1)
+    {
+        return CreateList(val, ListBuilder(val, start + 1, size));
+    }
+    else if( start >= size - 1)
+    {
+        return CreateList(val, EmptyList());
+    }
+}
 
-List* ListBuilder(int val, int start, int size);
 
-List* ListConstruct(int val, int size);
+//Creates a list of a specified size filled with the 'val' argument
+List* ListConstruct(int val, int size)
+{
+    ListBuilder(val, 0, size);
+}
 
-void ListDestroy(List** list);
+void ListDestroy(List** list)
+{
+    if(isEmpty(*list))
+    {
+        printf("List is empty\n");
+        *list = nullptr;
+        return;
+    }
+    if(!isEmpty(*list))
+    {
+        List* temp = (*list);
+        (*list) = (*list)->rest;
+        free(temp);
+        ListDestroy(list);
+    }
+    
+}
 
-List* EmptyList();
+//Useful function to create an empty list
+List* EmptyList()
+{
+    return nullptr;
+}
 
-int isEmpty(List* empty);
+//Checks if the element points to 0
+int isEmpty(List* empty)
+{
+    if(empty == nullptr)
+    {
+        return 1;
+    }
+    return 0;
+}
 
-int length(List* list, int startPos);
+//Recursive function used to implement list_length
+int length(List* list, int startPos)
+{
+    if(isEmpty(list))
+    {
+        return startPos;
+    }
+    length(list->rest, startPos + 1);
+}
 
-int list_length(List* list);
+//Returns the amount of elements on a list
+int list_length(List* list)
+{
+    length(list, 0);
+}
 
-List* getPosition(List* list, int start, int pos);
+//Recursive Function used to implement searchPos
+List* getPosition(List* list, int start, int pos)
+{
+    if(start == pos)
+    {
+        return list;
+    }
+    getPosition(list->rest, start + 1, pos);
+}
 
-List* searchPos(List* list, int pos);
+//Searches for the element in the specified position, set start to zero unless
+//You otherwise need to
+List* searchPos(List* list, int pos)
+{
+    return getPosition(list, 0, pos);
+}
 
-List* last(List* list);
+//Grabs the last element of a list
+List* last(List* list)
+{
+    if(isEmpty(list->rest))
+    {
+        return list;
+    }
+    last(list->rest);
+}
 
-void insert(List* list, int val, int pos);
+//Inserts a value at a zero-indexed position in the list
+void insert(List* list, int val, int ind)
+{
+    if(ind <= 0)
+    {
+        printf("Use the (join_head) function to insert at the start.\n");
+        return;
+    }
+    List* prev = searchPos(list, ind - 1);
+    List* next = prev->rest;
+    prev->rest = CreateList(val, next);
+    printf("Inserted element into list at position %d\n", ind);
+    prev = nullptr;
+    next = nullptr;
+}
 
-List* join_head(List* list, int val);
+//Returns a new list with the specified value at the start
+List* join_head(List* list, int val)
+{
+    return CreateList(val, list);
+}
 
-void join_list(List* first, List* second);
+//Add the elements of the second list to the first
+void join_list(List* first, List* second)
+{
+    if(!isEmpty(first->rest))
+    {
+        List* connect = last(first);
+        connect->rest = second;
+        connect = nullptr;
+    }
+    else if(isEmpty(first->rest))
+    {
+        first->rest = second;
+    }
+}
 
-void append_list(List* list, int val);
+//Adds an element to the end of the list
+void append_list(List* list, int val)
+{
+    List* last_element = last(list);
+    last_element->rest = CreateList(val, EmptyList());
 
-void replace(List* list, int pos, int val);
+    //Set pointers to null before returning
+    last_element = nullptr;
+}
 
-int DisplayList(List* display);
+//Replaces an element with the value of your choice
+void replace(List* list, int ind, int val)
+{
+    searchPos(list, ind)->data = val;
+}
+
+//Recursively displays all the elements of a list in a way that is easy to visualize
+int DisplayList(List* display)
+{
+    if(isEmpty(display))
+    {
+        printf("End of list.\n");
+        return 0;
+    }
+    printf("%d", display->data);
+    printf("->");
+    DisplayList(display->rest);
+}
